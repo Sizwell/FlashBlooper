c2b03f42edfec4da0173ed90da692e37e298302d
Sizwe Ncikana <sizwe.ncikana@gmail.com>
2023-08-01 13:38:54 +0200

    Sorted out MSSQL 403 Forbidden issue when writting to the Database

--- src/main/java/com/itech/Blooper/BlooperApplication.java
+++ src/main/java/com/itech/Blooper/BlooperApplication.java
@@ -6,15 +6,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-//import springfox.documentation.RequestHandler;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 
@@ -41,7 +32,6 @@ public class BlooperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlooperApplication.class, args);
-		System.setProperty("java.library.path", "bin/mssql-jdbc_auth-12.2.0.x64.dll");
 
 	}
 
